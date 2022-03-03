632d6a39bfab5e33dbeccf92c19653524f0cc79b
kitcanna <89905869+kitcanna@users.noreply.github.com>
2022-03-03 14:43:24 +0100

    Update Send Picture Button on MainPanel

--- src/model/pojo/Message.java
+++ src/model/pojo/Message.java
@@ -2,23 +2,36 @@ package model.pojo;
 
 import java.io.Serializable;
 
+import javax.swing.ImageIcon;
+
 public class Message implements PackageInterface, Serializable {
     
     private String message;
-
+    private ImageIcon icon;
 
     public Message(String message) {
         this.message = message;
     }
 
+    public Message(ImageIcon icon) {
+        this.icon = icon;
+    }
 
     public String getMessage() {
         return this.message;
     }
-
     public void setMessage(String message) {
         this.message = message;
     }
+
+    public ImageIcon getIconMessage() {
+        return this.icon;
+    }
+   
+    public void setIcon() {
+        this.icon = icon;
+    }
+
 
 
 }
